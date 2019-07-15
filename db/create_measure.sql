e06527ea5fe209bc2671123a825e0bbdc4aeeb94
Jose Reyes <josereyesch@gmail.com>
2019-07-15 11:40:28 +0200

    Add sql script to create measure table

---
+++ db/create_measure.sql
@@ -0,0 +1,10 @@
+-- Drop table
+
+-- DROP TABLE public.measure;
+
+CREATE TABLE public.measure (
+	datetime timestamp NOT NULL,
+	"parameter" varchar NOT NULL,
+	value float4 NOT NULL,
+	id serial NOT NULL
+);
