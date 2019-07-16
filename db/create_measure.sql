7f9a2ae6a9ca3660f69f8f3ca1335c11093e5888
Jorge López <jorge@Kira.local>
2019-07-16 12:01:22 +0200

    modified db schema v2

--- db/create_measure.sql
+++ db/create_measure.sql
@@ -1,11 +1,23 @@
--- Drop table
+DROP DATABASE IF EXISTS qoe_db;
 
--- DROP TABLE public.measure;
+CREATE DATABASE qoe_db;
+
+\connect qoe_db;
+
+DROP USER IF EXISTS qoe_user;
+	
+CREATE USER qoe_user WITH ENCRYPTED PASSWORD 'MPCLGP5432!';
+
+GRANT ALL PRIVILEGES ON DATABASE qoe_db TO qoe_user;
+
+DROP TABLE IF EXISTS measure;
 
 CREATE TABLE public.measure (
 	datetime timestamp NOT NULL,
-	"parameter" varchar NOT NULL,
+	parameter varchar NOT NULL,
 	value float4 NOT NULL,
 	id serial NOT NULL UNIQUE PRIMARY KEY,
 	groupid UUID not NULL
 );
+
+GRANT ALL PRIVILEGES ON TABLE measure TO oqoe_user;
